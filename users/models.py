ca38f90d83029215238d0510af9acffea8b8e998
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-05-19 15:45:52 +0300

    feat(users): Customize the user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Permission, Group
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    phone_number = PhoneNumberField(region='EG', blank=False, null=False, unique=True)
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+    wallet = models.IntegerField(default=0)
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=("groups"),
+        blank=True,
+        help_text=(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        related_name="user_groups",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=("user permissions"),
+        blank=True,
+        help_text=("Specific permissions for this user."),
+        related_name="user_permissions",
+        related_query_name="user",
+    )
