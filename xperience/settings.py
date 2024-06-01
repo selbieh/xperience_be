243d8e04fd4c5a985354bea2401a3d224a3b6eab
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-06-01 11:33:29 +0300

    fix: cors error

--- xperience/settings.py
+++ xperience/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "phonenumber_field",
     "rest_framework",
     "rest_framework.authtoken",
@@ -85,6 +86,7 @@ TWILIO_PHONE_NUMBER = env("TWILIO_PHONE_NUMBER")
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -170,3 +172,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=45)
 }
+
+
+CORS_ALLOW_ALL_ORIGINS = True
