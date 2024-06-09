f99198b33af0d53f88883e4c75a2351101129890
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-06-09 10:36:52 +0300

    feat(services): modify hotel_service_feature view permissions

--- services/views.py
+++ services/views.py
@@ -108,7 +108,12 @@ class ServiceOptionViewSet(viewsets.ModelViewSet):
 class HotelServiceFeatureViewSet(viewsets.ModelViewSet):
     queryset = HotelServiceFeature.objects.all()
     serializer_class = HotelServiceFeatureSerializer
-    permission_classes = [IsAdminUser]
+    def get_permissions(self):
+        if self.request.method in ['GET']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
 class CarMakeViewSet(viewsets.ModelViewSet):
