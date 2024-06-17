71c707d46ea5af8d8b3e1960d82be92ad035cf9d
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-06-17 12:43:04 +0300

    handling notifications

--- reservations/apps.py
+++ reservations/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class ReservationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reservations'
+
+    def ready(self) -> None:
+        from .signals import message_handler
---
+++ reservations/signals.py
@@ -0,0 +1,32 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Reservation
+from firebase_admin.messaging import Message, Notification
+from fcm_django.models import FCMDevice
+
+
+
+@receiver(post_save, sender=Reservation)
+def message_handler(sender, instance, created, **kwargs):
+    status = instance.status
+    if status == 'WAITING_FOR_PAYMENT':
+        title = ''
+        body= ''
+    elif status == 'CONFIRMED':
+        title = ''
+        body= ''
+    elif status == 'CANCELLED':
+        title = ''
+        body= ''
+    elif status == 'COMPLETED':
+        title = ''
+        body= ''
+
+    try:
+        register_tokens = FCMDevice.objects.filter(user=instance.user)
+        register_tokens.send_message(Message(notification=Notification(
+            title=title,
+            body=body
+        )))
+    except Exception as e:
+        print(e)
