95b0859e524303b3a7dd81bcee44353be9d39062
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-07-14 15:09:14 +0300

    point and points_price in the model

--- services/models.py
+++ services/models.py
@@ -36,7 +36,8 @@ class SubscriptionOption(AbstractBaseModel):
     car_service = models.ForeignKey(CarService, on_delete=models.CASCADE, related_name='subscription_options')
     duration_hours = models.IntegerField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    points = models.IntegerField(null=True, blank=True)
+    points = models.IntegerField(null=True, blank=True, default=0)
+    points_price = models.IntegerField(null=True)
 
 
 class CarImage(models.Model):
@@ -63,8 +64,9 @@ class HotelService(AbstractBaseModel):
     features = models.ManyToManyField(HotelServiceFeature)
     availability_start = models.DateField(null=True, blank=True)
     availability_end = models.DateField(null=True, blank=True)
-    points = models.IntegerField(null=True, blank=True)
     image = models.ImageField(upload_to='hotel_image/', blank=True, null=True)
+    points = models.IntegerField(null=True, blank=True, default=0)
+    points_price = models.IntegerField(null=True)
 
 
 class HotelImage(models.Model):
@@ -82,4 +84,5 @@ class ServiceOption(AbstractBaseModel):
     max_free = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     active = models.BooleanField(default=True)
+    points_price = models.IntegerField(null=True)
 
