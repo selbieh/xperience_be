82a7c79580e2f2228ad22b46576682119023b75d
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-08-03 14:57:38 +0300

    error

--- reservations/apps.py
+++ reservations/apps.py
@@ -1,8 +1,3 @@
 from django.apps import AppConfig
 
-class ReservationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reservations'
 
-    def ready(self) -> None:
-        from .signals import message_handler
